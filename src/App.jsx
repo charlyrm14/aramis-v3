8a4b89b6bed126299cd620b625c6e91b52ff0140
alfredxavi <alfredjavi28@gmail.com>
2024-06-11 13:04:27 -0600

    Se creo Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 
-import Dashboard from "@pages/Dashboard";
+import Dashboard from "@pages/Dashboard/Dashboard";
 import Reportes from "@pages/Reportes";
 import Login from "@pages/Login/Login";
 
